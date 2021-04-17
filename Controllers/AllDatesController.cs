ae027fb4d487604733425670b3fb4bdeea0f1c89
Ella <ella.vankirk@gmail.com>
2021-04-17 01:30:09 -0500

    A few changes

--- Controllers/AllDatesController.cs
+++ Controllers/AllDatesController.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TrackMyHabit.Data;
+using TrackMyHabit.Models;
 
 namespace TrackMyHabit.Controllers
 {
     public class AllDatesController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public AllDatesController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AllDates.ToListAsync());
+        }
+
+        public IActionResult Add()
+        {
+            AllDates dates = new AllDates();
+            return View(dates);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AllDates dates)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.AllDates.Add(dates);
+                _context.SaveChanges();
+                return Redirect("/AllDates");
+            }
+            return View("Add", dates);
         }
     }
 }
--- Models/AllDates.cs
+++ Models/AllDates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace TrackMyHabit.Models
     public class AllDates
     {
         public int ID { get; set; }
+        [Required]
         public DateTime Date { get; set; }
 
         public AllDates (DateTime date)
