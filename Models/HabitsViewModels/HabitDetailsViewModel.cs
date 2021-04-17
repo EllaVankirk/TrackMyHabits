30bb3e1e8f7370bc1fdabc4f6ec84bfcb660139d
Ella <ella.vankirk@gmail.com>
2021-04-17 06:03:53 -0500

    Added a habit detail viewmodel

---
+++ Models/HabitsViewModels/HabitDetailsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackMyHabit.Models.HabitsViewModels
+{
+    public class HabitDetailsViewModel
+    {
+        public int HabitID { get; set; }
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public string HabitInitial { get; set; }
+        public string DateText { get; set; }
+
+        public HabitDetailsViewModel() { }
+
+        public HabitDetailsViewModel(Habits theHabit, List<HabitsDates> habitDates)
+        {
+            HabitID = theHabit.ID;
+            Name = theHabit.Name;
+            Colour = theHabit.Colour;
+            HabitInitial = theHabit.HabitInitial;
+
+            DateText = "";
+
+            for(var i = 0; i < habitDates.Count; i++)
+            {
+                DateText = habitDates[i].AllDates.Date.ToString();
+
+                if (i < habitDates.Count - 1)
+                {
+                    DateText += ", ";
+                }
+            }
+        }
+
+    }
+}
